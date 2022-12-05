bc21bd67860c24411d3ff4d2fdfb5cf6c3b74abf
FirePenguin99 <gabriellewington@gmail.com>
2022-12-05 10:21:32 +0000

    added displays for pet stats, and the buttons change their values

--- script_events_2.js
+++ script_events_2.js
@@ -20,27 +20,33 @@ function Pet(name, type){
 function reduceFood (obj){
     obj.food -= 2;
     console.log("food: " + obj.food);
+    document.querySelector('#foodDisplay').textContent = "food: " + obj.food;
 }
 function reduceCleanliness (obj){
     obj.cleanliness -= 2;
     console.log("cleanliness: " + obj.cleanliness);
+    document.querySelector('#cleanlinessDisplay').textContent = "cleanliness: " + obj.cleanliness;
 }
 function reduceSleep (obj){
     obj.sleep -= 2;
     console.log("sleep: " + obj.sleep);
+    document.querySelector('#sleepDisplay').textContent = "sleep: " + obj.sleep;
 }
 
 function increaseFood (obj){
     obj.food += 2;
     console.log("food: " + obj.food);
+    document.querySelector('#foodDisplay').textContent = "food: " + obj.food;
 }
 function increaseCleanliness (obj){
     obj.cleanliness += 2;
     console.log("cleanliness: " + obj.cleanliness);
+    document.querySelector('#cleanlinessDisplay').textContent = "cleanliness: " + obj.cleanliness;
 }
 function increaseSleep (obj){
     obj.sleep += 2;
     console.log("sleep: " + obj.sleep);
+    document.querySelector('#sleepDisplay').textContent = "sleep: " + obj.sleep;
 }
 
 function calculateHappiness (obj){
@@ -48,31 +54,12 @@ function calculateHappiness (obj){
     console.log(obj.happiness);
 }
 
-// function attachListeners(){
-//     const feedButton = document.querySelector('#plusFood');
-//     feedButton.addEventListener('click', ()=>increaseFood(pet1));
-//     const cleanButton = document.querySelector('#plusClean');
-//     cleanButton.addEventListener('click', ()=>increaseCleanliness(pet1));
-//     const sleepButton = document.querySelector('#plusSleep');
-//     sleepButton.addEventListener('click', ()=>increaseSleep(pet1));
-//     const starveButton = document.querySelector('#subFood');
-//     starveButton.addEventListener('click', ()=>reduceFood(pet1));
-//     const dirtyButton = document.querySelector('#subClean');
-//     dirtyButton.addEventListener('click', ()=>reduceCleanliness(pet1));
-//     const tireButton = document.querySelector('#subSleep');
-//     tireButton.addEventListener('click', ()=>reduceSleep(pet1));
-// }
-
 function startGame(){
     
     console.log("Welcome to your Virtual Pet!")
     let pet1 = new Pet("goob", "cat");
 
     console.log("Say hello to your new " + pet1.type + ": " + pet1.name + "!")
-    
-    console.log(pet1.food);
-    reduceFood(pet1);
-    console.log(pet1.food);
 
     const feedButton = document.querySelector('#plusFood');
     feedButton.addEventListener('click', ()=>increaseFood(pet1));
@@ -90,7 +77,6 @@ function startGame(){
 
 window.addEventListener('load', ()=>{
 
-    //attachListeners();
     startGame();
 
 });
